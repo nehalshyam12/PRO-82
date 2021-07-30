31a7d049829c58bd3e741dc9dafe1d8677542b11
nehalshyam12 <75104500+nehalshyam12@users.noreply.github.com>
2021-07-30 20:21:37 +0530

    Add files via upload

---
+++ screens/CreatePost.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class CreatePost extends React.Component {
+    render() {
+        return (
+            <View style={{justifyContent: 'center', alignItems: 'center', marginTop: 250}}>
+                <Text>Create Post Screen</Text>
+            </View>
+        )
+    }
+}
